bb8014f7c582141d41e3d84262f655c089c715f1
Paweł Słowik <dolphinx@windowslive.com>
2013-04-24 17:18:41 +0200

    [Campaign] Fixed duplicates

--- Leveldata/Campaign/ascension/M01_Wastelands/datfiles.lua
+++
@@ -1,9 +0,0 @@
-Dictionaries = { -- lista plików z napisami
-	{
-		name="locale:leveldata/campaign/ascension/m01_wastelands.dat"
-	},
-	{
-		name="locale:leveldata/campaign/ascension/nis01a_m01_wastelands.dat"
-	}
-};
-
